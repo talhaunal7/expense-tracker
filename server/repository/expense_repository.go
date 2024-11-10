f320d20ffa58c7b1c6b8483820b9976e56c0739f
Talha Unal <talhaunal7@gmail.com>
2024-11-10 08:57:13 +0300

    create react project

--- server/repository/expense_repository.go
+++ server/repository/expense_repository.go
@@ -27,13 +27,11 @@ func (expenseRepo ExpenseRepositoryImpl) Add(expense entity.Expense) error {
 func (expenseRepo ExpenseRepositoryImpl) FindByID(id int) (entity.Expense, error) {
 	var expense entity.Expense
 	result := expenseRepo.db.Preload("Category").First(&expense, id)
-	//result := expenseRepo.db.First(&expense, id)
 	return expense, result.Error
 }
 
 func (expenseRepo ExpenseRepositoryImpl) FindAllByUserID(userID int) ([]entity.Expense, error) {
 	var expenses []entity.Expense
-	//result := expenseRepo.db.Preload("Category").Find(&expenses, userID)
 	result := expenseRepo.db.Preload("Category").Where("user_id = ?", userID).Find(&expenses)
 	return expenses, result.Error
 }
